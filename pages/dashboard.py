e314a97b755244680d9a2aa117313fec07ae48b9
ben-seacrest <ben@seacrestmarkets.io>
2025-10-22 18:20:53 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 def dashboard_page():
     first_name = st.session_state["first_name"]
 
-    st.title(f"Welcome, {first_name}")
+    st.subheader(f"Welcome, {first_name}", anchor=False)
 
 if __name__ == "__main__":
     dashboard_page()
