de0d21c97df8a62335b849b112534927f1b8bce6
ben-seacrest <ben@seacrestmarkets.io>
2025-10-22 18:20:35 +0100

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
 def dashboard_page():
-    user_id = st.session_state["user_id"]
     first_name = st.session_state["first_name"]
+
+    st.title(f"Welcome, {first_name}")
 
 if __name__ == "__main__":
     dashboard_page()
